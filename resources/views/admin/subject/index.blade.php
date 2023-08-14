5eeea85c68d97aa3d796da2f815968fe6a8885b4
idlkz <idlkz2019@gmail.com>
2023-08-14 17:38:39 +0600

    Commit Subject index

--- resources/views/admin/subject/index.blade.php
+++ resources/views/admin/subject/index.blade.php
@@ -1,13 +1,20 @@
 @extends('layouts.default')
 @section('content')
+    <x-layer-components.content-navbar
+        :title="'Список предметов'"
+        :subtitle="'Управление предметами'"
+    >
+        <a href="{{route("subject.create")}}" class="btn btn-primary mt-2 mt-xl-0">Создать</a>
 
+    </x-layer-components.content-navbar>
     <div class="col-lg-12 grid-margin stretch-card">
         <div class="card">
             <div class="card-body">
-
+                <livewire:subject.subject-table theme="tailwind" />
             </div>
         </div>
     </div>
 
 
 @endsection
+
