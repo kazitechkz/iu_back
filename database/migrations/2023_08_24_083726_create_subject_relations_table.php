8a733c5ff30c2d1c1edfeae595853a708a54258e
idlkz <idlkz2019@gmail.com>
2023-08-24 17:08:56 +0600

    Edited db tables

--- database/migrations/2023_08_24_083726_create_subject_relations_table.php
+++ database/migrations/2023_08_24_083726_create_subject_relations_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('subject_relations', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements("id");
             $table->foreignId('subject_id')->references('id')->on('subjects')->onDelete('cascade');
             $table->foreignId('relation_id')->references('id')->on('subjects')->onDelete('cascade');
             $table->timestamps();
