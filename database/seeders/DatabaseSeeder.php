8b1660981cb5026320ee40c820155e54e302ed21
idlkz <idlkz2019@gmail.com>
2023-09-01 10:36:52 +0600

    Commit Questions Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Question;
+use App\Models\SubjectContext;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -27,6 +29,8 @@ class DatabaseSeeder extends Seeder
                 GroupSeeder::class,
                 AttemptTypeSeeder::class,
                 TournamentStepSeeder::class,
+                SubjectContextSeeder::class,
+                QuestionsSeeder::class
             ]
         );
     }
