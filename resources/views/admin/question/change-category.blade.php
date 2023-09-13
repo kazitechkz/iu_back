f9fa1e6700b09611b91ffa20fd2645c7c9e6e2de
idlkz <idlkz2019@gmail.com>
2023-09-13 11:05:01 +0600

    Added Tutor Logic

--- resources/views/admin/question/change-category.blade.php
+++ resources/views/admin/question/change-category.blade.php
@@ -183,7 +183,7 @@
     <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
     <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_CHTML"></script>--}}
-<script type="text/javascript">
+<script async>
     MathJax.Hub.Config({
         tex2jax: { inlineMath: [['$$','$$']], displayMath: [['$$$$','$$$$']] }
     });
