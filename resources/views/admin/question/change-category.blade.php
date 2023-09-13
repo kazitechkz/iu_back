864c9289dc83696b8a5580e2efe87f588ce014c7
idlkz <idlkz2019@gmail.com>
2023-09-13 10:50:53 +0600

    Commit updated

--- resources/views/admin/question/change-category.blade.php
+++ resources/views/admin/question/change-category.blade.php
@@ -181,7 +181,7 @@
 @endsection
 @push('js')
     <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-    <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+    <script id="MathJax-script" async src="{{asset('js/mathjax.js')}}"></script>
 {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_CHTML"></script>--}}
 <script type="text/javascript">
     MathJax.Hub.Config({
