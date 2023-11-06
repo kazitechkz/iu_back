e84182d3ca37a62dfea55c656fcbbd49836f5680
idlkz <idlkz2019@gmail.com>
2023-11-06 15:04:32 +0600

    Commit cors

--- config/cors.php
+++ config/cors.php
@@ -20,7 +20,7 @@ return [
     'allowed_methods' => ['*'],
 
 //    'allowed_origins' => ['*'],
-    'allowed_origins' => ['http://localhost:4200', 'https://i-union.kz/'],
+    'allowed_origins' => ['http://localhost:4200', 'https://front.i-union.kz'],
 
     'allowed_origins_patterns' => [],
 
