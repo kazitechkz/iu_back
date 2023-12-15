07218c3693610d98c91912653faedd5997a4ea89
idlkz <idlkz2019@gmail.com>
2023-12-15 17:56:01 +0600

    Commit added StatsByUser

--- app/Http/Controllers/Admin/StatisticController.php
+++ app/Http/Controllers/Admin/StatisticController.php
@@ -11,11 +11,25 @@ class StatisticController extends Controller
 {
     public function statsOnQuestions()
     {
-        return view('admin.statistics.on-questions');
+        try {
+            if (auth()->user()->can("stats-by-user index")) {
+                return view('admin.statistics.on-questions');
+            }
+        } catch (\Exception $exception) {
+            toastr()->error($exception->getMessage(), "Error");
+            return redirect()->route("home");
+        }
     }
 
     public function statsOnUserContents()
     {
-        return view('admin.statistics.user-content-stats');
+        try {
+            if (auth()->user()->can("stats-by-user index")) {
+                return view('admin.statistics.user-content-stats');
+            }
+        } catch (\Exception $exception) {
+            toastr()->error($exception->getMessage(), "Error");
+            return redirect()->route("home");
+        }
     }
 }
