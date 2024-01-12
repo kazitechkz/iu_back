92eb459dfd20305ccb8e72ea13c8c719c04436d4
kazitechkz <kazitech2023@gmail.com>
2024-01-12 11:01:32 +0600

    Added Many Feature

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('FORCE_HTTPS'),false) {
-            URL::forceScheme('https');
-        }
+
     }
 }
