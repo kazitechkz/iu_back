6d735f4e06345d99faa5f17561420b32672149c6
IDLKZ <idlkz2019@gmail.com>
2024-02-03 16:41:49 +0600

    Commit TestController

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -77,6 +77,11 @@ class TestController extends Controller
         }
     }
 
+    public function payboxResultURL(Request $request)
+    {
+        $this->getResult($request);
+        return redirect('http://localhost:4200/dashboard/plan-mode');
+    }
     public function payboxResultSuccess(Request $request)
     {
         $this->getResult($request);
@@ -96,6 +101,43 @@ class TestController extends Controller
     }
 
     public function getResult(Request $request)
+    {
+        dd($request->all());
+        $response = $this->_payService->getResultStatus($request);
+        $content = json_decode($response->content(), true);
+        if ($content['pg_status'] == 'ok') {
+            $order = PayboxOrder::where('order_id', $request['pg_order_id'])->first();
+            if ($order) {
+                $user = User::find($order->user_id);
+                $order->status = 1;
+                $order->save();
+                foreach ($order->plans as $item) {
+                    $plan = Plan::find($item);
+                    if(PlanSubscription::where(["subscriber_id"=>$order->user_id,"plan_id"=>$plan->id])->first()){
+                        // Check subscriber to plan
+                        if(!$user->isSubscribedTo($plan->id))
+                        {
+                            $user->subscription($plan->tag)->renew();
+                        }
+                    }
+                    else{
+                        $user->newSubscription(
+                            $plan->tag, // identifier tag of the subscription. If your application offers a single subscription, you might call this 'main' or 'primary'
+                            $plan, // Plan or PlanCombination instance your subscriber is subscribing to
+                            $plan->name, // Human-readable name for your subscription
+                            $plan->description // Description
+                        );
+                    }
+                }
+            } else {
+                abort(500, 'Неизвестная ошибка');
+            }
+        } else {
+            throw new \Exception('Неизвестная ошибка');
+        }
+    }
+
+    public function getResultForSuccessURL(Request $request)
     {
         $response = $this->_payService->getResultStatus($request);
         $content = json_decode($response->content(), true);
--- routes/api.php
+++ routes/api.php
@@ -215,6 +215,7 @@ if(env("IS_API",true)){
     Route::get("/test",[\App\Http\Controllers\Api\TestController::class,"test"]);
     Route::post("/send-whatsapp",[\App\Http\Controllers\Api\TestController::class,"sendWhatsapp"]);
     Route::post("/paybox",[\App\Http\Controllers\Api\TestController::class,"paybox"]);
+    Route::post("/pay/result",[\App\Http\Controllers\Api\TestController::class,"payboxResultURL"]);
     Route::post("/pay/success",[\App\Http\Controllers\Api\TestController::class,"payboxResultSuccess"]);
     Route::post("/pay/failure",[\App\Http\Controllers\Api\TestController::class,"payboxResultFailure"]);
 }
