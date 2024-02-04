f92ee9ecfd4ec2d9674e65efd765dc4a855becdf
IDLKZ <idlkz2019@gmail.com>
2024-02-04 14:21:13 +0600

    Commit TestController

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -109,56 +109,29 @@ class TestController extends Controller
     }
     public function payboxResultSuccess(Request $request)
     {
-        $this->getResult($request);
-        return redirect('http://localhost:4200/dashboard/plan-mode');
+        $desc = $this->getResult($request);
+        $link = "http://localhost:4200/dashboard/plan-mode?success=1&desc=".$desc;
+        return redirect($link);
     }
 
     public function payboxResultFailure(Request $request)
     {
-        $this->getResult($request);
-//        PayboxOrder::where('order_id', 562)->firstOrCreate([
-//            'order_id' => 562,
-//            'price' => 990,
-//            'user_id' => 999,
-//            'status' => 1
-//        ]);
-        return redirect('http://localhost:4200/dashboard/plan-mode');
+        return redirect('http://localhost:4200/dashboard/plan-mode?error=1');
     }
 
     public function getResult(Request $request)
     {
-        dd($request->all());
         $response = $this->_payService->getResultStatus($request);
         $content = json_decode($response->content(), true);
         if ($content['pg_status'] == 'ok') {
             $order = PayboxOrder::where('order_id', $request['pg_order_id'])->first();
-            if ($order) {
-                $user = User::find($order->user_id);
-                $order->status = 1;
-                $order->save();
-                foreach ($order->plans as $item) {
-                    $plan = Plan::find($item);
-                    if(PlanSubscription::where(["subscriber_id"=>$order->user_id,"plan_id"=>$plan->id])->first()){
-                        // Check subscriber to plan
-                        if(!$user->isSubscribedTo($plan->id))
-                        {
-                            $user->subscription($plan->tag)->renew();
-                        }
-                    }
-                    else{
-                        $user->newSubscription(
-                            $plan->tag, // identifier tag of the subscription. If your application offers a single subscription, you might call this 'main' or 'primary'
-                            $plan, // Plan or PlanCombination instance your subscriber is subscribing to
-                            $plan->name, // Human-readable name for your subscription
-                            $plan->description // Description
-                        );
-                    }
-                }
+            if ($order->status == 1) {
+                return $order->description;
             } else {
-                abort(500, 'Неизвестная ошибка');
+                return null;
             }
         } else {
-            throw new \Exception('Неизвестная ошибка');
+            return null;
         }
     }
 
